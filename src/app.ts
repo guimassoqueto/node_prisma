9f8c914104b70c80354a70a6fa628c3a9552b1c5
gui.massoqueto <gui.massoqueto@gmail.com>
2022-06-15 08:10:28 -0300

    pt1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-console.log("ok")
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+/*
+  await prisma.user.create({
+    data: {
+        name: 'Guilherme',
+        email: 'gui.massoqueto@gmail.com',
+        posts: {
+            create: { title: 'Hello World' },
+        },
+        profile: {
+            create: { bio: 'Guilherme\'s bio' }
+        },
+    },
+  });
+*/
+
+  const all_users = await prisma.user.findMany({
+    include: {
+        posts: true,
+        profile: true,
+    }
+  });
+
+  return all_users
+
+}
+
+main()
+  .then(users => {
+    for ( const user of users ) {
+        console.log(user.profile);
+    }
+  })
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
